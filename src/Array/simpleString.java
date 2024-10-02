4422a1f190b8571d71b6fc1ba457dc52a37e0ec8
Vikash <rakesh25paswan@gmail.com>
2024-10-02 12:06:19 +0530

    simple string

---
+++ src/Array/simpleString.java
@@ -0,0 +1,15 @@
+package Array;
+
+import java.util.Scanner;
+
+public class simpleString {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String s = sc.next();
+		
+		System.out.println(s);
+	}
+
+}
